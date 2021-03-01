274d3a64bc9159aa9ef940d8d61ef6b3ae91eb9d
hyunkim <hyunkim@hyunkimui-MacBookPro.local>
2021-03-01 09:42:48 +0900

    #0.5 How does React Native Work?

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>No problem!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gold',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color:"green",
+    fontSize: 30
+  }
 });
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # fokin-weather
 Learning React Native By Building Foking Weather App
+
+npm start -> 다른 터미널창에서 expo login -> run ios simulator(같은 와이파이에 연결되었는지 확인)
+
+# Live Reload
+원하는 내용을 바꾸면 즉시 리프레쉬되면서 화면에 나타남
+수동으로 하고 싶다면 아이폰화면에서 command + R or ctrl + R
+개발자메뉴open -> command + D
