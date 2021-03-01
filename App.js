af08bbdc90018d3092d74188a91fa442b001beaa
hyunkim <hyunkim@gmail.com>
2021-03-01 10:38:52 +0900

    #1.1 Loading Screen

--- App.js
+++ App.js
@@ -1,26 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Loading from "./Loading";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.redview} />
-      <View style={styles.blueview} />
-    </View>
-  );
+  return <Loading/>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  redview:{
-    flex:1,
-    backgroundColor:"red"
-  },
-  blueview:{
-    flex:2,
-    backgroundColor:"blue"
-  }
-});
+
